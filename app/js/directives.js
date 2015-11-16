244a8e7f3b65d04d6d19991dea50b0d86d92ac2e
Luke Phillips <luke.phillips@nextissuemedia.com>
2015-11-16 15:15:08 -0800

    Add dots widget for attributes / abilities / merits.

--- app/js/directives.js
+++ app/js/directives.js
@@ -24,3 +24,54 @@ lytekDirectives.directive("visnetwork", function() {
       }
     };
 });
+
+lytekDirectives.directive("dots", function() {
+  return {
+    restrict: "E",
+    require: "^ngModel",
+    template: "<div></div>",
+    replace: true,
+    scope: {
+      ngModel: "=",
+    },
+    link: function(scope, element, attrs) {
+      for (var i = 0; i < 5; i++) {
+        var imgElem = document.createElement("img");
+        if (i < scope.ngModel) {
+          imgElem.setAttribute("src", "img/dot.png");
+        }
+        else {
+          imgElem.setAttribute("src", "img/dot_empty.png");
+        }
+
+        imgElem.setAttribute("width", "12");
+        imgElem.setAttribute("height", "12");
+        imgElem.onclick = (function(properties) {
+          var childNodes = this.parentNode.childNodes;
+          var newVal = 0;
+          var elemFound = false;
+          for (var index = 0; index < childNodes.length; index++) {
+            var dotElem = childNodes[index];
+
+            if (!elemFound) {
+              newVal = index + 1;
+            }
+            if(childNodes[index] == this) {
+              elemFound = true;
+            }
+            if (index < newVal) {
+              dotElem.setAttribute("src", "img/dot.png");
+            }
+            else {
+              dotElem.setAttribute("src", "img/dot_empty.png");
+            }
+          }
+          scope.ngModel = newVal;
+          scope.$apply();
+          console.log("new Atrribute: " + scope.ngModel);
+        });
+        element[0].appendChild(imgElem);
+      };
+    }
+  }
+});
