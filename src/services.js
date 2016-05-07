553bd914ab806ab395411a5f5f0c5ad58a2eb76f
Luke Phillips <luke.phillips@nextissuemedia.com>
2016-05-07 15:10:08 -0700

    Remove unneeded script tags.

--- src/services.js
+++ src/services.js
@@ -1,19 +1,25 @@
-/*jslint node: true*/
-'use strict';
+import angular from 'angular';
 
 /* Services */
 
 var lytekServices = angular.module('lytekServices', ['ngResource']);
-export default lytekServices;
+export
+default lytekServices;
 
-lytekServices.factory('Charms', ['$resource', function ($resource) {
-  return $resource('charms/:ability.json', {}, {});
-}]);
+lytekServices.factory('Charms', ['$resource',
+    function($resource) {
+        return $resource('charms/:ability.json', {}, {});
+    }
+]);
 
-lytekServices.factory('MartialArts', ['$resource', function ($resource) {
-  return $resource('martial-arts/martial-arts.json', {}, {});
-}]);
+lytekServices.factory('MartialArts', ['$resource',
+    function($resource) {
+        return $resource('martial-arts/martial-arts.json', {}, {});
+    }
+]);
 
-lytekServices.factory('Merits', ['$resource', function ($resource) {
-  return $resource('merits/merits.json', {}, {});
-}]);
+lytekServices.factory('Merits', ['$resource',
+    function($resource) {
+        return $resource('merits/merits.json', {}, {});
+    }
+]);
